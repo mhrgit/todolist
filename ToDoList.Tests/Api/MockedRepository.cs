a264937a609d0dd0649f960f796680e578a70eb4
Mahir <mhrydngl@gmail.com>
2016-11-23 23:45:18 +0000

    Moving back-end unit tests to separate project

---
+++ ToDoList.Tests/Api/MockedRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ToDoList.Domain.Interfaces;
+using Task = ToDoList.Domain.Entities.Task;
+
+namespace ToDoList.Api.Tests
+{
+    public class MockedRepository : IRepository
+    {
+        private IList<Task> Context { get; set; }
+
+        public MockedRepository()
+        {
+            Context = new List<Task>();
+            Context.Add(new Task { Id = 1, Name = "Go to swimming"});
+            Context.Add(new Task { Id = 2, Name = "Go to shopping" });
+            Context.Add(new Task { Id = 3, Name = "Go to cinema" });
+        }
+
+        public IEnumerable<Task> GetAll()
+        {
+            return Context;
+        }
+
+        public Task GetById(int id)
+        {
+            return Context.FirstOrDefault(i => i.Id == id);
+        }
+
+        public void Add(Task task)
+        {
+            Context.Add(task);
+        }
+
+        public void Update(Task task)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Task task)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ToDoList.Tests/Api/ToDoListControllerTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using ToDoList.Api.Controllers;
+using ToDoList.Data.Repositories;
+using ToDoList.Domain.Interfaces;
+using Task = ToDoList.Domain.Entities.Task;
+
+namespace ToDoList.Api.Tests
+{
+    [TestFixture]
+    public class ToDoListControllerTests
+    {        
+        [Test]
+        public void Controller_Should_Return_Everything_When_GetAll_Method_Called()
+        {
+            var controller = new ToDoListController(new MockedRepository());
+
+            Assert.AreEqual(3, controller.GetAll().Count());
+        }
+
+        [Test]
+        public void Controller_Should_Return_Task_When_Get_Method_Called()
+        {
+            var controller = new ToDoListController(new MockedRepository());
+
+            Assert.AreEqual(2, controller.Get(2).Id);
+        }
+
+        [Test]
+        public void Controller_Should_Add_New_Task_When_Add_Method_Called()
+        {
+            var controller = new ToDoListController(new MockedRepository());
+
+            controller.Add(new Task {Id=4, Name = "Play football"});
+
+           Assert.AreEqual(4, controller.GetAll().Count());
+        }
+    }
+}
