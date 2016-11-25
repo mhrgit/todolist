954ad3b31e706b31f0f01de6b0c788e22ce9741e
Mahir <mhrydngl@gmail.com>
2016-11-25 19:41:03 +0000

    Javascript unit tests moved to separate web project

---
+++ ToDoList.WebTests/Scripts/dependencies/todo-service.js
@@ -0,0 +1,20 @@
+﻿todoApp.constant('TASKS_ENDPOINT', 'http://localhost:51001/api/todolist/')
+    .factory('TasksService', function($http, $q, TASKS_ENDPOINT) {
+
+        function extract(result) {
+            return result.data;
+        }
+
+        function getTasks() {
+            return $http.get(TASKS_ENDPOINT + 'getAll').then(extract);
+        }
+
+        function addTask(task) {
+            return $http.post(TASKS_ENDPOINT + 'add', task).then(extract);
+        };
+
+        return {
+            getTasks: getTasks,
+            addTask: addTask    
+        }
+    });
